d5afc22d96ab6f783a7587b7f895077324fce7f2
Rupesh Ranjan <rupeshranjan409@gmail.com>
2021-01-17 18:01:10 +0530

    Multiple changes * Add icons for action * Add running time * Add search button * Css modifications

--- src/js/App.js
+++ src/js/App.js
@@ -1,15 +1,28 @@
 import '../css/App.css';
 import TaskTable from './TaskTable.js';
+import React, { Component } from 'react';
 
-export default function App() {
-  return (
-    <>
-    <div>
-      <div className="App-header"> To-Do App </div>
-      <br /> 
-      <br />
-      <TaskTable/>
-    </div>
-    </>
-  );
+export default class App extends Component {
+  state = {}
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        currTime : new Date().toLocaleString()
+      })
+    }, 1000)
+  }
+
+  render() {
+      return (
+        <div>
+          <div className="App-header">
+            <div className="To-Do"> To-Do App </div>
+            <div className="Time-Block"> {this.state.currTime} </div>
+            <input className="Search" type="text" placeholder="Search.." />
+          </div>
+          <br/>
+          <TaskTable/>
+        </div>
+      );
+  }
 }
--- src/js/TaskTable.js
+++ src/js/TaskTable.js
@@ -52,7 +52,55 @@ const tasks = [
     "added_on": 1610862610385,
     "due_by": 1610963610385,
     "status": "Done"
-  }
+  },
+  {
+      "id": 7,
+      "title": "Add action column with update and delete buttons",
+      "description": "Add action column with update and delete buttons",
+      "added_on": 1610862610385,
+      "due_by": 1610963610385,
+      "status": "Done"
+    },
+    {
+      "id": 8,
+      "title": "Remove description from columns",
+      "description": "Remove description from columns",
+      "added_on": 1610862610385,
+      "due_by": 1610963610385,
+      "status": "TODO"
+    },
+    {
+      "id": 9,
+      "title": "Make title clickable, and pop up all details on click",
+      "description": "Make title clickable, and pop up all details on click",
+      "added_on": 1610862610385,
+      "due_by": 1610963610385,
+      "status": "TODO"
+    },
+    {
+      "id": 10,
+      "title": "Make buttons actionable",
+      "description": "make delete button actionable",
+      "added_on": 1610862610385,
+      "due_by": 1610963610385,
+      "status": "Done"
+    },
+    {
+      "id": 11,
+      "title": "Make buttons actionable",
+      "description": "make update button actionable",
+      "added_on": 1610862610385,
+      "due_by": 1610963610385,
+      "status": "Done"
+    },
+    {
+      "id": 12,
+      "title": "delete button in action",
+      "description": "should delete row with delete button",
+      "added_on": 1610862610385,
+      "due_by": 1610963610385,
+      "status": "Done"
+    }
 ];
 
 export default class TaskTable extends Component {
@@ -70,25 +118,38 @@ export default class TaskTable extends Component {
 
   updateHandler = () => console.log("update");
 
+  markAsDoneHandler = (taskId) => {
+    let temp = this.state.tasks;
+    for(let i=0; i<temp.length; i++) {
+        if(temp[i].id === taskId) {
+            temp[i].status = "Done";
+        }
+    }
+    this.setState({'tasks': temp});
+  }
+
   render() {
   return (
     <div>
       <table className="TaskTable">
-        <tbody>
-          <tr className="HeadRow"> 
-            <td className="Value TitleColumn"> Title </td>
-            <td className="Value DescriptionColumn"> Description </td>
-            <td className="Value OtherColumns"> Added On </td>
-            <td className="Value OtherColumns"> Due By </td>
-            <td className="Value OtherColumns"> Status </td>
-            <td className="Value OtherColumns" colSpan={2}> Actions </td>
+        <thead className="HeadRow">
+          <tr>
+            <th className="Value TitleColumn"> Title </th>
+            <th className="Value DescriptionColumn"> Description </th>
+            <th className="Value OtherColumns"> Added On </th>
+            <th className="Value OtherColumns"> Due By </th>
+            <th className="Value OtherColumns"> Status </th>
+            <th className="Value OtherColumns" colSpan={3}> Actions </th>
           </tr>
-            {this.state.tasks.map(task => 
-              <TaskRow key={task.id}
-                       updateHandler={this.updateHandler} 
-                       deleteHandler={this.deleteHandler}
-                       {...task} />
-            )}
+        </thead>
+        <tbody>
+          {this.state.tasks.map(task =>
+            <TaskRow key={task.id}
+                   updateHandler={this.updateHandler}
+                   deleteHandler={this.deleteHandler}
+                   markAsDoneHandler={this.markAsDoneHandler}
+                   {...task} />
+          )}
         </tbody>
       </table>
     </div>
@@ -99,7 +160,9 @@ export default class TaskTable extends Component {
 function TaskRow(props) {
   const added_on = moment.unix(props.added_on / 1000).format('dddd, MMMM Do, YYYY h:mm A');
   const due_by = moment.unix(props.due_by / 1000).format('dddd, MMMM Do, YYYY h:mm A');
-  
+const doneVisibility = (props.status === "TODO");
+  const editVisibility = (props.status === "TODO");
+
   return (
     <tr className="DataRow"> 
       <td className="Value TitleColumn"> {props.title} </td>
@@ -107,11 +170,22 @@ function TaskRow(props) {
       <td className="Value OtherColumns"> {added_on} </td>
       <td className="Value OtherColumns"> {due_by} </td>
       <td className="Value OtherColumns"> {props.status} </td>
-      <td className="Value OtherColumns"> 
-        <Button text="update" action={props.updateHandler} taskId={props.id}/> 
+      <td className="Value OtherColumns">
+        <Button id="Update"
+                action={props.updateHandler}
+                visibility={editVisibility}
+                taskId={props.id} />
       </td>
       <td className="Value OtherColumns">
-        <Button text="delete" action={props.deleteHandler} taskId={props.id}/> 
+        <Button id="Remove"
+                action={props.deleteHandler}
+                taskId={props.id} />
+      </td>
+      <td className="Value OtherColumns">
+        <Button id="Done"
+                action={props.markAsDoneHandler}
+                visibility={doneVisibility}
+                taskId={props.id} />
       </td>
     </tr>
   );
