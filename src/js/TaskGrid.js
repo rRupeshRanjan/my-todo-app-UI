364530b7d9987b574f1548070ef554019978a7e2
Rupesh Ranjan <rupeshranjan409@gmail.com>
2021-01-24 18:14:43 +0530

    Adding action with ag-grid and hooking api-call for delete

--- src/js/TaskGrid.js
+++ src/js/TaskGrid.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import moment from 'moment';
+import axios from 'axios';
+import ButtonCellRenderer from './renderer/ButtonCellRenderer.js';
 
 import '../css/TaskGrid.css';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
+// TODO: Remove this. Keeping just for testing
 const tasks = [
   {
     "id": 1,
@@ -22,103 +25,48 @@ const tasks = [
     "added_on": 1610862610385,
     "due_by": 1610963610385,
     "status": "TODO"
-  },
-  {
-    "id": 3,
-    "title": "Make title clickable, and pop up all details on click",
-    "description": "Make title clickable, and pop up all details on click",
-    "added_on": 1610862610385,
-    "due_by": 1610963610385,
-    "status": "TODO"
-  },
-  {
-    "id": 4,
-    "title": "Make buttons actionable",
-    "description": "make delete button actionable",
-    "added_on": 1610862610385,
-    "due_by": 1610963610385,
-    "status": "Done"
-  },
-  {
-    "id": 5,
-    "title": "Make buttons actionable",
-    "description": "make update button actionable",
-    "added_on": 1610862610385,
-    "due_by": 1610963610385,
-    "status": "Done"
-  },
-  {
-    "id": 6,
-    "title": "delete button in action",
-    "description": "should delete row with delete button",
-    "added_on": 1610862610385,
-    "due_by": 1610963610385,
-    "status": "Done"
-  },
-  {
-      "id": 7,
-      "title": "Add action column with update and delete buttons",
-      "description": "Add action column with update and delete buttons",
-      "added_on": 1610862610385,
-      "due_by": 1610963610385,
-      "status": "Done"
-    },
-    {
-      "id": 8,
-      "title": "Remove description from columns",
-      "description": "Remove description from columns",
-      "added_on": 1610862610385,
-      "due_by": 1610963610385,
-      "status": "TODO"
-    },
-    {
-      "id": 9,
-      "title": "Make title clickable, and pop up all details on click",
-      "description": "Make title clickable, and pop up all details on click",
-      "added_on": 1610862610385,
-      "due_by": 1610963610385,
-      "status": "TODO"
-    },
-    {
-      "id": 10,
-      "title": "Make buttons actionable",
-      "description": "make delete button actionable",
-      "added_on": 1610862610385,
-      "due_by": 1610963610385,
-      "status": "Done"
-    },
-    {
-      "id": 11,
-      "title": "Make buttons actionable",
-      "description": "make update button actionable",
-      "added_on": 1610862610385,
-      "due_by": 1610963610385,
-      "status": "Done"
-    },
-    {
-      "id": 12,
-      "title": "delete button in action",
-      "description": "should delete row with delete button",
-      "added_on": 1610862610385,
-      "due_by": 1610963610385,
-      "status": "Done"
-    }
+  }
 ];
 
 export default class TaskGrid extends Component {
-    state = {};
-    dateRenderer = (data) => { return moment(data.value).format('MM/DD/YYYY HH:mm') };
+    state = {}
+
+    dateRenderer = (data) => { return moment(data.value).format('MM/DD/YYYY HH:mm') }
+
+    markAsDoneHandler = (taskId) => {
+        // TODO: Make api-call
+        let tasksCopy = this.state.tasks.slice();
+        for(let i=0; i<tasksCopy.length; i++) {
+            if(tasksCopy[i].id === taskId) {
+                tasksCopy[i].status = "Done";
+            }
+        }
+        this.setState({ tasks : tasksCopy });
+        this.gridApi.redrawRows();
+    }
+
+    deleteHandler = (taskId) => {
+        axios.delete(`http://localhost:8080/task/${taskId}`);
+        axios.get(`http://localhost:8080/tasks`)
+             .then(res => this.setState({ tasks: res.data}));
+    }
+
+    editHandler = (taskId) => {
+        // TODO: Implement this
+        console.log("please implement me first, huh!!")
+    }
 
     onGridReady = (params) => {
-        this.gridApi = params.gridApi;
-        this.gridColumnApi = params.gridColumnApi;
-    };
+        this.gridApi = params.api;
+        this.gridColumnApi = params.columnApi;
+    }
 
-    componentDidMount() {
-        this.setState({
-            tasks: tasks,
-            gridApi: null,
-            gridColumnApi: null,
+    constructor(props) {
+        super(props);
+        this.state = {
+            frameworkComponents: {
+                ButtonCellRenderer: ButtonCellRenderer,
+            },
             defaultColDef: {
                 flex: 1,
                 resizable: true
@@ -155,22 +103,39 @@ export default class TaskGrid extends Component {
                     colId: 'status',
                     sortable: true,
                     filter: true,
-                }
-            ]
-        });
+                },
+                {
+                    headerName: 'Actions',
+                    cellRenderer: 'ButtonCellRenderer',
+                    cellRendererParams: {
+                        message: 'Hello world',
+                        deleteAction: this.deleteHandler,
+                        markDoneAction: this.markAsDoneHandler,
+                        editAction: this.editHandler,
+                    },
+                },
+            ],
+        };
+    }
+
+    componentDidMount() {
+        axios.get(`http://localhost:8080/tasks`)
+             .then(res => this.setState({ tasks: res.data}));
     }
 
     render() {
         return (
-            <div className="ag-theme-alpine">
+            <div className='ag-theme-alpine'>
                 <AgGridReact
                     domLayout='autoHeight'
-                    onGridReady = {this.onGridReady}
                     rowSelection = 'multiple'
                     rowData = {this.state.tasks}
                     columnDefs = {this.state.columnDefs}
-                    defaultColDef={this.state.defaultColDef}>
-                </AgGridReact>
+                    defaultColDef={this.state.defaultColDef}
+                    immutableData={false}
+                    frameworkComponents={this.state.frameworkComponents}
+                    onGridReady = {this.onGridReady}
+                />
             </div>
         )
     };
---
+++ src/js/renderer/ButtonCellRenderer.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+import '../../css/Button.css';
+import edit from '../../images/edit.png'
+import remove from '../../images/remove.png'
+import done from '../../images/done.png'
+
+export default class ButtonCellRenderer extends Component {
+    render() {
+        return (
+            <div>
+                <button
+                    title='edit'
+                    onClick={ () => this.props.editAction(this.props.data.id) }
+                    style={{ cursor: getCursorForAction(this.props.data.status) }}
+                    disabled={isButtonDisabled(this.props.data.status)}>
+                    <img src={getImageForButton('edit')} alt={'edit'} className="icon" />
+                </button>
+                <button
+                    title='remove'
+                    onClick={ () => this.props.deleteAction(this.props.data.id) }>
+                    <img src={getImageForButton('remove')} alt={'remove'} className="icon" />
+                </button>
+                <button
+                    title='done'
+                    onClick={ () => this.props.markDoneAction(this.props.data.id) }
+                    style={{ cursor: getCursorForAction(this.props.data.status) }}
+                    disabled={isButtonDisabled(this.props.data.status)}>
+                    <img src={getImageForButton('done')} alt={'done'} className="icon" />
+                </button>
+            </div>
+        );
+    }
+}
+
+function getImageForButton(action) {
+    switch (action) {
+    case "edit":
+        return edit;
+    case "remove":
+        return remove;
+    case "done":
+        return done;
+    default:
+        return ' ';
+    }
+}
+
+function getCursorForAction(status) {
+    return status === 'Done' ? 'not-allowed' : 'allowed';
+}
+
+function isButtonDisabled(status) {
+    return getCursorForAction(status) !== 'allowed' ? true: false;
+}
