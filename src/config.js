9d44fe7c4246c6785829903b8cb073e28dfe3cc0
Rupesh <rupeshranjan409@gmail.com>
2021-11-20 13:49:45 +0530

    Added config and ability to compute pnl on quantity

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export var configs = {
+    "basePriceUrl": "http://localhost:8080/digital-gold-prices/",
+    "getHistoricalPricesPath": "/get-historical-prices?size=100",
+    "savePricesPath": "/save-historical-prices",
+    "pnlCalculateUrl": "http://localhost:8080/pnl/calculatePnl?buyDate={0}&sellDate={1}"
+}
