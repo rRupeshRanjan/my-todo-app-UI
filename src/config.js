5e5fca79f857f9ece0a7f091a5f4ec2d1832712a
Rupesh Ranjan <rupeshranjan409@gmail.com>
2022-08-13 11:44:34 +0200

    Candlestick (#2)
    * Moving to candlestick graph, removing pnl calculation
    * Adding snapshot of graph
    * Removing unused dependencies
    * Add current price to UI
    * Set x-axis intervals

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export var configs = {
     "getPricesUrl": "http://localhost:8080/digital-gold-prices/get-historical-prices?size={0}",
+    "getCurrentPriceUrl": "http://localhost:8080/digital-gold-prices/get-current-price",
     "savePricesUrl": "http://localhost:8080/digital-gold-prices/save-historical-prices",
     "pnlCalculateUrl": "http://localhost:8080/pnl/calculatePnl?buyDate={0}&sellDate={1}"
 }
